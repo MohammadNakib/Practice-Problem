9f78d67786277c2f1780c50cc75f3ca4415c0277
Md. Nakibul Islam <65423018+MohammadNakib@users.noreply.github.com>
2021-08-06 03:07:50 +0600

    Add files via upload

---
+++ LightOJ 1000 Greetings.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int T,a,b,i=1;
+    scanf("%d",&T);
+    while(i<=T)
+    {
+        scanf("%d%d",&a,&b);
+        printf("Case %d: %d\n",i,a+b);
+        i++;
+    }
+    return 0;
+}
